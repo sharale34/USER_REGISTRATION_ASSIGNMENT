c04f862c04a43d721441577a662278f5a02139e3
sharale34 <sourabhharale8000@gmail.com>
2020-10-01 06:35:17 +0530

    Added: Welcome Message Is Added

---
+++ UserRegistration.java
@@ -0,0 +1,10 @@
+package com.BridgeLabz.UserRegistration;
+
+public class UserRegistration
+{
+	  public static void main(String[] args)
+	  {
+	  // TODO Auto-generated method stub
+	  System.out.println("Welcome To User Registration Program");
+	  }
+}
