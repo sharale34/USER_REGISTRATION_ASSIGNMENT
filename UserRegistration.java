312529fbf22b90613eec1c2b98c9b64562974095
sharale34 <sourabhharale8000@gmail.com>
2020-10-01 06:42:24 +0530

    Added: Functionality To Check Valid First Name Is Added

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,10 +1,35 @@
 package com.BridgeLabz.UserRegistration;
 
-public class UserRegistration
-{
-	  public static void main(String[] args)
-	  {
-	  // TODO Auto-generated method stub
-	  System.out.println("Welcome To User Registration Program");
-	  }
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+
+	     public static void main(String[] args) {
+		 // TODO Auto-generated method stub
+		 System.out.println("Welcome To User Registration Program");
+	     Scanner sc = new Scanner (System.in);
+	     System.out.println("Enter the first name :");
+	     String firstName = sc.nextLine();
+	     String pattern ="^[A-Z][a-z]{2,}";
+	     Pattern p = Pattern.compile(pattern);
+	     Matcher m1 = p.matcher(firstName);
+	     if(m1.matches())
+	     {
+	     	System.out.println("The first name of the user is: "+firstName);
+	     }
+	     else
+	     {
+	    	 if(firstName.length() <= 2 )
+		     {
+		     	System.out.println("The first name should have minimum 3 characters");
+		     }
+	    	 else 
+	    	 {
+	    	 System.out.println("Enter a valid input");
+	    	 }
+	     }
+	}
+
 }
